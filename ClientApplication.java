fdadd9934e0df98b551082b256b6643e772d51a3
Chris Wang <chris_wang@icloud.com>
2018-05-13 17:40:45 +0100

    report.txt

--- ClientApplication.java
+++ ClientApplication.java
@@ -1,9 +1,7 @@
 import client.*;
-import common.Database;
 
 public class ClientApplication {
     private Client initialise() {
-        Database database = new Database();
         return new Client();
     }
 
